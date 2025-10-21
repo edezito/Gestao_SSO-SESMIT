ac4eb4f235e825e241d67b23587b5ce7e688f12f
Filipe Rocha <felipe.kw@hotmail.com>
2025-10-21 15:21:58 -0300

    Corrige default de data_agendamento em ExameModel

--- backend/src/infrastructure/model/exame_model.py
+++ backend/src/infrastructure/model/exame_model.py
@@ -1,7 +1,5 @@
-from time import timezone
 from src.config.database import db
-from datetime import datetime, timedelta
-
+from datetime import datetime
 class Exame(db.Model):
     __tablename__ = "exames"
     
@@ -11,14 +9,14 @@ class Exame(db.Model):
     
     colaborador_id = db.Column(db.Integer, db.ForeignKey("usuarios.id"), nullable=False)
     tipo_exame = db.Column(db.Enum('ADMISSIONAL', 'PERIODICO', 'RETORNO', 'DEMISSIONAL', name="tipo_exame"), nullable=False)
-    data_agendamento = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc))
+    data_agendamento = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     data_realizacao = db.Column(db.DateTime)
     observacoes = db.Column(db.Text)
     
     @property
     def status(self):
         if not self.data_realizacao:
-            if self.data_agendamento < datetime.now(timezone.utc)():
+            if self.data_agendamento < datetime.utcnow():
                 return "VENCIDO"
             return "PENDENTE"
         return "REALIZADO"
